8a572332adc9fcf5972fbbc9ab4448a9d2159b88
Gabor Karsay <gabor.karsay@gmx.at>
2021-11-23 20:22:26 +0100

    Add test for extractTimestamp()
    This is a proof of concept for tests using UNO. It creates an empty
    writer document, writes to it and tests a methods with that content.

--- tests/test_parlatype_utils.py
+++ tests/test_parlatype_utils.py
@@ -1,9 +1,22 @@
 import unittest
+import uno
+from unotest import UnoRemoteConnection
 
 import parlatype_utils as pt_utils
 
 
 class TestUtils(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls._uno = UnoRemoteConnection({'program': 'soffice',
+                                        'verbose': False})
+        cls._uno.setUp()
+        cls.document = cls._uno.openEmptyWriterDoc()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls._uno.tearDown()
+
     def test_isValidCharacter(self):
         """
         Test _isValidCharacter()
@@ -19,6 +32,36 @@ class TestUtils(unittest.TestCase):
         invalid_characters = ['A', 'a', 'z', 'Z', '#', ';', ' ']
         for character in invalid_characters:
             self.assertFalse(pt_utils._isValidCharacter(character))
+
+    def test_extractTimestamp(self):
+        """
+        Test extractTimestamp()
+        """
+
+        # Get the empty document and insert a string (overwriting)
+        doc = self.__class__._uno.getDoc()
+        text = doc.Text
+        cursor = text.createTextCursor()
+        text.insertString(cursor, "#2:48#", True)
+
+        # Get current selections
+        controller = doc.getCurrentController()
+        selections = controller.getSelection()
+
+        # Get first (and in this case only) selection
+        sel = selections.getByIndex(0)
+
+        # Get cursor of selection (it is at the end of the inserted text)
+        cursor = sel.getText().createTextCursorByRange(sel)
+
+        # Move cursor (without expanding selection)
+        cursor.goLeft(1, False)
+
+        # Set selection
+        controller.select(cursor)
+
+        timestamp = pt_utils.extractTimestamp(controller)
+        self.assertEqual(timestamp, '2:48')
 
 
 if __name__ == '__main__':
